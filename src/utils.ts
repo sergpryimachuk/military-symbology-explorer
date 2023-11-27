aba7f9a8db551013d78fd99d6892b6b83a6c4b97
sergpryimachuk <serg@pryimach.uk>
2023-11-27 17:35:25 +0100

    Re-arrange files.

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-export const groupBy = <T extends object, K extends keyof T>(
-  arr: T[],
-  key: K
-) =>
-  arr.reduce((acc, item) => {
-    acc.set(item[key], [...(acc.get(item[key]) || []), item]);
-    return acc;
-  }, new Map<T[K], T[]>());
